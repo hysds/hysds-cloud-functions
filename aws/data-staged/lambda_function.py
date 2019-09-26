198d17cc46d70ef5cd13a3c185a7842a8c34d7d0
Michael Cayanan <42812746+mcayanan@users.noreply.github.com>
2019-09-26 07:52:04 -0700

    HC-104: Update ingest lambda to support job type to regex mapping (#2)
    * HC-104: Update ingest lambda to support regex to job type mapping
    
    * remove yaml as it is not needed
    
    * Compile the regex
    
    * print out matching job type info
    
    * update
    
    * Update docs

--- aws/data-staged/lambda_function.py
+++ aws/data-staged/lambda_function.py
@@ -10,6 +10,38 @@ signal_file_suffix = None
 if "SIGNAL_FILE_SUFFIX" in os.environ:
     signal_file_suffix = os.environ['SIGNAL_FILE_SUFFIX']
 
+
+def __get_job_type_info(data_file, job_types, default_type, default_release,
+                   default_queue):
+    """
+    Determine the job type.
+
+    :param data_file: The data file being ingested.
+    :param job_types: A mapping of job types to a regex.
+    :param default_type: Default job type.
+    :param default_release: Default job release version.
+    :param: default_queue: Default job queue.
+    :return: Function will try to match the given data file to one of 
+    the types specified in the job type mapping. If no mapping exists 
+    or a match could not be found, then it will use the default job
+    type, release, and queue.
+    """
+    for type in job_types.keys():
+        regex = job_types[type]['PATTERN']
+        print("Checking if {} matches {}".format(regex, data_file))
+        match = re.search(regex, data_file)
+        if match:
+            release = job_types[type]['RELEASE']
+            queue = job_types[type]['QUEUE']
+            print("Data file '{}' matches job type info: "
+                  "type: {}, release: {}, queue: {}".format(
+                data_file, type, release, queue))
+            return type, release, queue
+    print("Could not match data file '{}' to a given job type: {}. "
+          "Using default job type info".format(data_file, job_types.keys()))
+    return default_type, default_release, default_queue
+
+
 def lambda_handler(event, context):
     '''
     This lambda handler calls submit_job with the job type info
@@ -48,9 +80,17 @@ def lambda_handler(event, context):
     # data file
     id = data_file = os.path.basename(ds_url)
     
-    #submit mozart jobs to update ES
-    job_type = os.environ['JOB_TYPE'] # e.g. "INGEST_L0A_LR_RAW"
-    job_release = os.environ['JOB_RELEASE'] # e.g. "gman-dev"
+    # submit mozart jobs to update ES
+    default_job_type = os.environ['JOB_TYPE'] # e.g. "INGEST_L0A_LR_RAW"
+    default_job_release = os.environ['JOB_RELEASE'] # e.g. "gman-dev"
+    default_queue = os.environ['JOB_QUEUE']
+    job_types = {}
+    if 'JOB_TYPES' in os.environ:
+        job_types = json.loads(os.environ['JOB_TYPES'])
+    job_type, job_release, queue = __get_job_type_info(data_file, job_types,
+                                                       default_job_type,
+                                                       default_job_release,
+                                                       default_queue)
     job_spec = "job-%s:%s" % (job_type, job_release)
     job_params = {
         "id": id,
@@ -58,7 +98,6 @@ def lambda_handler(event, context):
         "data_file": data_file,
         "prod_met": md,
     }
-    queue = os.environ['JOB_QUEUE'] # eg.g "factotum-job_worker-large"
     tags = ["data-staged"]
 
     # submit mozart job
